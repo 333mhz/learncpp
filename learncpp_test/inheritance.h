7232b6087022c6026b7fe910b4dcc74f034e6387
333mhz <chendezheng1996@aliyun.com>
2019-08-18 23:50:34 +0800

    20190818

--- learncpp_test/inheritance.h
+++ learncpp_test/inheritance.h
@@ -1,15 +1,15 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef INHERITANCE_H
+#define INHERITANCE_H
 
 #include "header.h"
 
 class Person
 {
-    public:
+private:
     string m_name;
     int m_age;
-
-    Person(string n,int a):m_name(n),m_age(a)
+public:
+    Person(string n = "UNKNOW",int a = -1):m_name(n),m_age(a)
     {
     }
 
@@ -19,14 +19,125 @@ class Person
 
 class BaseballPlayer:public Person
 {
-    public:
+private:
     double m_battingAverage;
     int m_homeRuns;
-
-    BaseballPlayer(double ba = 0.0,int hr =0):m_battingAverage(ba),m_homeRuns(hr)
+public:
+    BaseballPlayer(string n,int a,double ba = 0.0,int hr =0):Person(n,a),m_battingAverage(ba),m_homeRuns(hr)
     {}
 
+    double getBattingAverage() const { return m_battingAverage; }
+    int getHomeRuns() const { return m_homeRuns; }
 };
 
+class A
+{
+public:
+    A(int a)
+    {
+        std::cout << "A: " << a << '\n';
+    }
+};
+ 
+class B: public A
+{
+public:
+    B(int a, double b)
+    : A(a)
+    {
+        std::cout << "B: " << b << '\n';
+    }
+};
+ 
+class C: public B
+{
+public:
+    C(int a , double b , char c)
+    : B(a, b)
+    {
+        std::cout << "C: " << c << '\n';
+    }
+};
+ 
+
+class Base
+{
+    private:
+   int m_id;
+
+    public:
+    Base(int id=0)
+        : m_id(id)
+    {
+    }
+ 
+    int getId() const { return m_id; }
+};
+ 
+class Derived: public Base
+{
+private:
+    double m_cost;
+
+public:
+    Derived(double cost=0.0, int id = 0)
+        : m_cost(cost), Base(id)
+    {
+    }
+ 
+    double getCost() const { return m_cost; }
+};
+
+class Fruit
+{
+    private:
+    string m_name;
+    string m_color;
+
+    public:
+    Fruit(string n,string c):m_name(n), m_color(c)
+    {
+    }
+
+    const string getName() const
+    {
+        return m_name;
+    }
+    const string getColor() const
+    {
+        return m_color;
+    }
+};
+
+class Apple:public Fruit
+{
+    private:
+    double m_fiber;
+
+    public:
+    Apple(string n, string c, double f):Fruit(n,c),m_fiber(f)
+    {}
+
+    friend ostream& operator<<(ostream& out,const Apple& a)
+    {
+        out << "Apple("<<a.getName()<<","<<a.getColor()<<","<<a.m_fiber<<")"<<endl;
+
+        return out;
+    }
+};
+
+class Banana:public Fruit
+{
+    private:
+    public:
+    Banana(string n, string c):Fruit(n,c)
+    {}
+    friend ostream& operator<<(ostream& out,const Banana& b)
+    {
+        out << "Banana("<<b.getName()<<","<<b.getColor()<<")"<<endl;
+
+        return out;
+    }
+};
 #endif
 
--- learncpp_test/test.cpp
+++ learncpp_test/test.cpp
@@ -3,17 +3,11 @@
 
 int main()
 {
-    // Create a new BaseballPlayer object
+	const Apple a("Red delicious", "red", 4.2);
+	std::cout << a;
+ 
+	const Banana b("Cavendish", "yellow");
+	std::cout << b;
 
-    BaseballPlayer joe;
-
-    // Assign it a name (we can do this directly because m_name is public)
-
-    joe.m_name = "Joe";
-
-    // Print out the name
-
-    std::cout << joe.getName() << '\n'; 
-	// use the getName() function we've acquired from the Person base class
-	return 0;
+    return 0;
 }
