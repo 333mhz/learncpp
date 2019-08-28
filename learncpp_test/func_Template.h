a18b90c8b1f6e664339fec7f069d1d42b023beb0
dzchen <dzchen@hyosung.com>
2019-08-28 17:31:09 +0800

    20190828 functemp 1

---
+++ learncpp_test/func_Template.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTEMP_H
+#define FUNCTEMP_H
+
+#include "header.h"
+
+
+#endif
--- learncpp_test/test.cpp
+++ learncpp_test/test.cpp
@@ -1,9 +1,16 @@
 #include "header.h"
-#include "virtual_Functions.h"
+#include "func_Template.h"
+template<typename type>
+
+const type& max(const type& x,const type& y)
+{
+    return (x>y)?x:y; 
+}
 
 int main()
 {
+    
+    double i = max(1.234,2.3456);
+    cout << i;
 
-    quiz2c();
- 	
 }
